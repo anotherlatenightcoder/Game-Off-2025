45400a017d890f67c0bad0b37564dc0d38e74655
anotherlatenightcoder <webejame@gmail.com>
2025-11-25 19:59:13 +0200

    Generate random codes for ships (im too lazy to make custom ones)

--- Assets/_GameOff2025/_Scripts/Managers/ShipManager.cs
+++ Assets/_GameOff2025/_Scripts/Managers/ShipManager.cs
@@ -69,6 +69,8 @@ namespace Route24.GameOff
         
         public void OnShipReadyForInspection(ShipController ship)
         {
+            _currentShipProfile.EntryCode = Random.Range(0, 10000).ToString("D4");
+            
             Debug.Log($"[GameManager] {_currentShipProfile.ShipName} ready for inspection.");
             _gameManager.OnShipReadyForInspection();
             _eventHub?.Publish(new ShipArrivedForInspectionEvent(currentShipIndex, _currentShipProfile));
