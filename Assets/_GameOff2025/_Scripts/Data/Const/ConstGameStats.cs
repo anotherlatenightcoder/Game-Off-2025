e8a7db10a8a1166b0c973ae1229ac1640df59a76
anotherlatenightcoder <webejame@gmail.com>
2025-12-01 21:58:29 +0200

    Penalty reduced to 10 credits

--- Assets/_GameOff2025/_Scripts/Data/Const/ConstGameStats.cs
+++ Assets/_GameOff2025/_Scripts/Data/Const/ConstGameStats.cs
@@ -3,7 +3,7 @@ namespace Route24.GameOff
     public static class ConstGameStats
     {
         public const int ApprovedShipSalary = 5;
-        public const int WrongApprovedPenalty = 15;
+        public const int WrongApprovedPenalty = 10;
         
         public const int DelayBeforeDayStart = 5;
         public const int DayTime = 360;
